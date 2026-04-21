10ba94a1b0b30dd176b1f881bacd1dabbe8acea0
mingyang2013 <mingyang2013@gmail.com>
2026-04-21 18:53:12 +0800

    fix: 更新图片路径和vite配置
    修正默认头像的路径引用，从绝对路径改为相对路径
    调整vite配置，添加路径别名以支持更灵活的引用方式

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   root: 'src',
   server: { port: 3000 },
-  publicDir: '../pic',
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, '..'),
+    },
+  },
 });
